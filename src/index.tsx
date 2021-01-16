255b189d65e366ec036f9505722ffc6485aac927
xeusteerapat <xeus085@gmail.com>
2021-01-16 15:11:02 +0700

    [ADD] search user comonent to index page

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GuestList from './state/GuestList';
+import UserSearch from './state/UserSearch';
 
 const App = () => {
   return (
     <div className='container'>
       <h1>Hi There</h1>
       <GuestList />
+      <UserSearch />
     </div>
   );
 };
